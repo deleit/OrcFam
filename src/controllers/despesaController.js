bba2bf9328445c18c9324b9afbe771112e2a00f8
Deivison Leite <dlmacedo9@gmail.com>
2022-08-08 16:07:33 -0300

    Adição do campo de categoria às despesas

--- src/controllers/despesaController.js
+++ src/controllers/despesaController.js
@@ -2,23 +2,29 @@ import Despesa from "../models/Despesa.js";
 
 class DespesaController {
   static listaDespesas = (req, res) => {
-    Despesa.find((err, despesas) => {
-      if (err)
-        res.status(500).send({ error: `Falha ao listar despesas: ${err.message}` });
-      else
-        res.status(200).json(despesas)
-    })
+    Despesa
+      .find()
+      .populate('categoria', '-_id')
+      .exec((err, despesas) => {
+        if (err)
+          res.status(500).send({ error: `Falha ao listar despesas: ${err.message}` });
+        else
+          res.status(200).json(despesas)
+      });
   }
 
   static listaDespesaPorId = (req, res) => {
     let id = req.params.id;
 
-    Despesa.findById(id, (err, despesa) => {
-      if (err)
-        res.status(400).send({ error: `ID de despesa não localizada: ${err.message}` });
-      else
-        res.status(200).json(despesa);
-    });
+    Despesa
+      .findById(id)
+      .populate('categoria', '-_id')
+      .exec((err, despesa) => {
+        if (err)
+          res.status(400).send({ error: `ID de despesa não localizada: ${err.message}` });
+        else
+          res.status(200).json(despesa);
+      });
   }
 
   static cadastraDespesa = (req, res) => {
--- src/models/Despesa.js
+++ src/models/Despesa.js
@@ -4,7 +4,8 @@ const despesaSchema = new mongoose.Schema({
   id: { type: String },
   descricao: { type: String, required: true },
   valor: { type: Number, required: true },
-  data: { type: Date, default: Date.now }
+  data: { type: Date, default: Date.now },
+  categoria: { type: mongoose.Types.ObjectId, ref: 'Categoria', default: '62f1404215e3c153c95efe9a' }
 }, { versionKey: false });
 
 const Despesa = mongoose.model('Despesa', despesaSchema);
